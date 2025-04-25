eea34dbf74be46c0838c4a2f33067fa127952589
Yash Patil <40046473+ypatil12@users.noreply.github.com>
2025-04-25 10:06:47 -0400

    chore: reset

---
+++ crossChainExample/contracts/script/DeployCertificateVerifier.sol
@@ -0,0 +1,92 @@
+// SPDX-License-Identifier: UNLICENSED 
+pragma solidity ^0.8.12;
+
+import {ProxyAdmin} from "eigenlayer-middleware/lib/openzeppelin-contracts/contracts/proxy/transparent/ProxyAdmin.sol";
+import {TransparentUpgradeableProxy} from "eigenlayer-middleware/lib/openzeppelin-contracts/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
+import {ITransparentUpgradeableProxy} from "eigenlayer-middleware/lib/openzeppelin-contracts/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
+import {EmptyContract} from "eigenlayer-core/test/mocks/EmptyContract.sol";
+
+import {BLSCertificateVerifier} from "eigenlayer-middleware/src/crossChain/BLSCertificateVerifier.sol";
+
+import {OperatorSet} from "eigenlayer-core/contracts/libraries/OperatorSetLib.sol";
+
+import "forge-std/Test.sol";
+import "forge-std/Script.sol";
+import "forge-std/StdJson.sol";
+
+contract DeployCertificateVerifier is Script, Test {
+    ProxyAdmin public proxyAdmin;
+    EmptyContract public emptyContract;
+
+    BLSCertificateVerifier public certificateVerifier;
+    BLSCertificateVerifier public certificateVerifierImplementation;
+
+    struct AVSDeployment {
+        address serviceManager;
+    }
+    
+
+    function run(
+        string memory avsConfigPath,
+        uint256 operatorSetId,
+        uint32 maxTableStaleness
+    ) public {
+        // read the json file
+        string memory avsConfig = vm.readFile(avsConfigPath);
+        AVSDeployment memory avsDeployment = AVSDeployment({
+            serviceManager: stdJson.readAddress(avsConfig, ".serviceManager")
+        });
+
+        uint256 deploymentBlock = block.number;
+        vm.startBroadcast();
+
+        OperatorSet memory operatorSet = OperatorSet({
+            avs: avsDeployment.serviceManager,
+            operatorSetId: operatorSetId
+        });
+
+        // Deploy ProxyAdmin
+        l2ProxyAdmin = new ProxyAdmin();
+
+        emptyContract = new EmptyContract();
+
+        // Deploy proxy
+        certificateVerifier = new BLSCertificateVerifier(
+            address (new TransparentUpgradeableProxy(address(emptyContract), address(proxyAdmin), ""))
+        );
+
+        // Deploy implementation
+        certificateVerifierImplementation = new BLSCertificateVerifier(
+            address (new TransparentUpgradeableProxy(address(emptyContract), address(proxyAdmin), ""))
+        );
+
+        // TODO: make this upgradeable
+        // Upgrade Proxy
+        proxyAdmin.upgradeAndCall(
+            ITransparentUpgradeableProxy(payable(address(certificateVerifier))),
+            address(certificateVerifierImplementation),
+            abi.encodeWithSelector(
+                BLSCertificateVerifier.initialize.selector,
+                operatorSet,
+                msg.sender,
+                maxTableStaleness,
+                msg.sender
+            )
+        );
+
+        vm.stopBroadcast();
+
+        string memory output = "crossChainDeployment";
+        vm.serializeAddress(output, "certificateVerifier", address(certificateVerifier));
+        vm.serializeAddress(output, "certificateVerifierImplementation", address(certificateVerifierImplementation));
+        vm.serializeAddress(output, "proxyAdmin", address(proxyAdmin));
+        vm.serializeAddress(output, "emptyContract", address(emptyContract));
+        vm.serializeUint(output, "deploymentBlock", deploymentBlock);
+
+        string memory finalJson = vm.serializeString(output, "object", output);
+
+        vm.createDir("./script/output", true);
+        vm.writeJson(finalJson, "./script/output/certificateVerifier_deploy_output.json");
+    }
+
+}
